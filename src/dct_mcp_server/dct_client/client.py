90b60e1cb4e94de74305f243debb11503c397d40
vinay.byrappa <vinay.byrappa@delphix.com>
2025-12-04 20:49:42 +0530

    Project version change
    PR URL: https://www.github.com/delphix/dxi-mcp-server/pull/10

--- src/dct_mcp_server/dct_client/client.py
+++ src/dct_mcp_server/dct_client/client.py
@@ -33,7 +33,7 @@ class DCTAPIClient:
         try:
             version = importlib.metadata.version("dct-mcp-server")
         except importlib.metadata.PackageNotFoundError:
-            version = "2025.1.0-Preview"
+            version = "2025.0.1.0-preview"
 
         # Default headers
         self.headers = {
